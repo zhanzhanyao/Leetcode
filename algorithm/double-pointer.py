f8faf32cfe779c0caee3f3930b441c9a5231bc92
zhanzhanyao <940105829@qq.com>
2022-03-08 14:35:17 +0800

    Formatting only

--- algorithm/double-pointer.py
+++ algorithm/double-pointer.py
@@ -12,6 +12,7 @@ class DoublePoint:
     def two_sum(self, numbers: List[int], target: int) -> List[int]:
         """
         Two sum - input array is sorted
+
         :param numbers: one 1-indexed array of integers that is sorted in non-decreasing order.
         :param target: find 2 numbers such that they add up to a specific target number.
         :return: an array made by the indices of two numbers
@@ -32,6 +33,7 @@ class DoublePoint:
     def judge_squaresum(self, c: int) -> bool:
         """
         Sum of Square Numbers
+
         :param c: given a non-nagative integer.
         :return: whether there're two integers a and b such that a^2 + b^2 = c
 
@@ -54,6 +56,7 @@ class DoublePoint:
     def reverse_vowels(self, s: str) -> str:
         """
         Reverse all vowels in a string
+
         :param s: given a string s contains vowels 'a','e','i','o','u' and capital letters
         :return: reverse only all the vowels in s and return it.
 
@@ -79,6 +82,7 @@ class DoublePoint:
     def valid_palindrome(self, s: str) -> bool:
         """
         Valid palindrome
+
         :param s: given a string s
         :return: return true if the s can be palindrome after deleting at most one character.
 
@@ -110,6 +114,7 @@ class DoublePoint:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> List[int]:
         """
         Merge sorted array
+
         :param nums1: non-decreasing order
         :param m: the number of nums1, exclude 0 in the end of nums1
         :param nums2: non-decreasing order
@@ -144,6 +149,7 @@ class DoublePoint:
     def find_longestword(self, s: str, dictionary: List[str]) -> str:
         """
         Longest word in dictionary through deleting
+
         :param s:
         :param dictionary:
         :return: the longest string in the dictionary that can be formed by deleting some of the given string characters.
